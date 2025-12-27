3e5314a79265e93945753f508f149efd9ff3a681
yousefgharib335 <23p0159@eng.asu.edu.eg>
2025-12-27 22:17:07 +0200

    Delete ansys directory

--- ansys/project_files/dp0/SYS/MECH/cleanup-ansys-yousef-gharib-11752.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set LOCALHOST=%COMPUTERNAME%
-if /i "%LOCALHOST%"=="Omar-Eltoutongy" (taskkill /f /pid 14496)
-if /i "%LOCALHOST%"=="Omar-Eltoutongy" (taskkill /f /pid 11752)
-
-del /F cleanup-ansys-Omar-Eltoutongy-11752.bat
